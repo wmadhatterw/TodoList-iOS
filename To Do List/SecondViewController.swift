12ccb07a7444d5974cb218a133b2d1d0c9ca1d6b
Seth Walton <sdwalton83@gmail.com>
2018-08-21 13:45:32 -0500

    to do list works

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var itemTextField: UITextField!
+    
+    @IBAction func add(_ sender: Any) {
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        
+        if let tempItems = itemsObject as? [String]{
+            items = tempItems
+            items.append(itemTextField.text!)
+            
+        } else {
+            
+            items = [itemTextField.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        itemTextField.text = ""
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
